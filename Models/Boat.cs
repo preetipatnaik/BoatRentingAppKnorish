f8a2ffe99966138b266dcb7b29ce4c1e342e7792
preetipattnaik <preetileena2002@yahoo.com>
2020-09-05 11:32:39 +0530

    Add project files.

---
+++ Models/Boat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoatRentingAppKnorish.Models
+{
+    public class Boat
+    {
+        //public  MyProperty { get; set; }
+        //public int MyProperty { get; set; }
+        //public int MyProperty { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BoatRentingAppKnorish.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
